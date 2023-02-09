2886bd82658debd70d376b7c8c8d0a33a743bb0d
evanhinde <200529232@student.georgianc.on.ca>
2023-02-09 17:00:56 -0500

    Added scene

--- src/main/java/com/example/wk5memorygame/MemoryGameController.java
+++ src/main/java/com/example/wk5memorygame/MemoryGameController.java
@@ -1,4 +1,26 @@
 package com.example.wk5memorygame;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+
 public class MemoryGameController {
+
+
+    @FXML
+    private Label correctLabel;
+
+    @FXML
+    private FlowPane flowPane;
+
+    @FXML
+    private Label guessesLabel;
+
+    @FXML
+    void playAgain(ActionEvent event) {
+
+    }
+
 }
